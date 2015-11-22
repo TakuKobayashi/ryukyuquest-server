cdfb35d87fab9b4b56891a207932c425065c304a
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-11-22 10:43:33 +0900

    mysql へのconnection情報を追加

--- config/connections.js
+++ config/connections.js
@@ -42,10 +42,12 @@ module.exports.connections = {
   ***************************************************************************/
   someMysqlServer: {
     adapter: 'sails-mysql',
-    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_MYSQL_USER',
-    password: 'YOUR_MYSQL_PASSWORD',
-    database: 'YOUR_MYSQL_DB'
+    host: 'localhost',
+    //port: 3306,
+    user: 'root',
+    password: '',
+    database: 'RyukyuQuest',
+    charset: 'utf8',
   },
 
   /***************************************************************************
